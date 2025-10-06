467b42e847a02c20d8e2e9f086192b1825c6dc07
Rumesh Kaluarachchi <rumeshlakmal186@gmail.com>
2025-10-06 08:16:07 +0530

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,11 +59,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Rumesh Kaluarachchi";
+            document.title = "Rumesh Kaluarachchi";
             $("#favicon").attr("href", "../assets/images/rumesh1.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
+            document.title = "Rumesh Kaluarachchi";
             $("#favicon").attr("href", "../assets/images/rumesh1.png");
         }
     });
